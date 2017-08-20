894b339b97205bd26d515d3427f22b34518ba562
Cameron Korb <cameronk@mit.edu>
2017-08-20 20:11:03 +0200

    It works. Only tie breaker for head to head wins in a two way tie included because it's the only one needed for this data

--- elimination.py
+++ elimination.py
@@ -1,6 +1,7 @@
 import pandas as pd
-
+from pprint import pprint as pprint
 import config
+import numpy as np
 
 
 # TODO: change so instead of passing dfs, it's using pickle files
@@ -64,6 +65,16 @@ def compute_wins(scores, teams):
 
     return df
 
+def compute_results_teamwise(data, scores, teams):
+    data['Games Against'] = data.groupby(['Team_Name', 'Team Played']).cumcount() + 1
+    data['Wins Against'] = data.groupby(['Team_Name', 'Team Played'])['Win'].cumsum()
+
+    data = data.sort_values(by=['Team_Name', 'Date'], ascending=[1, 1]).reset_index().drop('index', axis=1)
+    data = data.drop_duplicates(subset=['Team_Name', 'Team Played'], keep='last')
+
+    data = data[['Team_Name', 'Team Played', 'Games Against', 'Wins Against']]
+
+    return data
 
 def compute_games_left(data, scores, teams):
     data['Games Played'] = data.groupby('Team_Name').cumcount() + 1
@@ -91,19 +102,57 @@ def get_minimum_top_8_wins(data):
     return data
 
 
-def get_possible_wins_naive(data):
+
+
+def get_possible_wins(data):
     data['Max Possible Wins'] = data['Total Wins'] + data['Games Left']
-    data['Eliminated'] = data['Max Possible Wins'] < data['Wins To Beat']
+    data['Eliminated'] = 'Playoffs'
+    data.loc[data['Max Possible Wins'] < data['Wins To Beat'], 'Eliminated'] = 'yes'
+    data.loc[data['Max Possible Wins'] == data['Wins To Beat'], 'Eliminated'] = 'tie'
 
     return data
 
+def tie_breaker(teams, results_teamwise):
+    #8th place team makes it automatically
+    if len(teams) == 1:
+        return teams.get_value(0,'Team_Name')
 
-def get_elimination_dates(teams, data):
+    #2 way tie for 8th place, head to head wins is first tie breaker, don't need more for 2016/2017
+    if len(teams) == 2:
+        result = results_teamwise.loc[(results_teamwise['Team_Name'] == teams.iloc[0]['Team_Name']) & \
+                                    (results_teamwise['Team Played'] == teams.iloc[1]['Team_Name'])].reset_index()
+        if result.get_value(0,'Wins Against')/ result.get_value(0,'Games Against') > .5:
+            return result.get_value(0,'Team_Name')
+        elif result.get_value(0,'Wins Against')/ result.get_value(0,'Games Against') < .5:
+            return result.get_value(0,'Team Played')
+        else:
+            #Go to next tie breaker rule...
+            pass
+
+    #3 way tie for 7th/8th place, division leader is first tie breaker
+    #didn't happen in 2016/2017
+    if len(teams) == 3:
+        pass
+
+
+def get_elimination_dates(teams, data, results_teamwise):
     data = data.drop_duplicates(subset=['Team_Name', 'Eliminated'])
-    print (data)
+
     data = data.drop_duplicates(subset=['Team_Name'], keep='last')
-    data.loc[data['Eliminated'], 'Elimination Date'] = data['Date']
+    data.loc[data['Eliminated'] == 'yes', 'Elimination Date'] = data['Date']
+    data.loc[data['Eliminated'] == 'tie', 'Elimination Date'] = data['Date']
     data['Elimination Date'] = data['Elimination Date'].fillna('Playoffs')
+
+
+    east_ties = data.loc[(data['Eliminated'] == 'tie') & (data["Conference_id"] == 'East')][['Team_Name']].reset_index()
+    west_ties = data.loc[(data['Eliminated'] == 'tie') & (data["Conference_id"] == 'West')][['Team_Name']].reset_index()
+    east_tie_win = tie_breaker(east_ties, results_teamwise)
+    west_tie_win = tie_breaker(west_ties, results_teamwise)
+
+    data.loc[(data['Eliminated'] == 'tie') & (data['Conference_id'] == 'East') \
+                    & (data['Team_Name'] == east_tie_win), 'Elimination Date'] = 'Playoffs'
+    data.loc[(data['Eliminated'] == 'tie') & (data['Conference_id'] == 'West') \
+                    & (data['Team_Name'] == west_tie_win), 'Elimination Date'] = 'Playoffs'
 
     data = data[['Team_Name', 'Elimination Date']]
 
@@ -116,10 +165,11 @@ def main():
     scores_df = get_season_scores()
     teams_df = get_team_info()
     df = compute_wins(scores_df, teams_df)
+    results_teamwise = compute_results_teamwise(df, scores_df, teams_df)
     df = compute_games_left(df, scores_df, teams_df)
     df = get_minimum_top_8_wins(df)
-    df = get_possible_wins_naive(df)
-    get_elimination_dates(teams_df, df)
+    df = get_possible_wins(df)
+    get_elimination_dates(teams_df, df, results_teamwise)
     # east_teams = (teams_df[teams_df['Conference_id'] == 'East'], 'east')
     # west_teams = (teams_df[teams_df['Conference_id'] == 'West'], 'west')
     # for conference in [east_teams, west_teams]:
